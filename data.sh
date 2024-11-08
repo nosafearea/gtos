ad090a73b9831fc866ff5e85adec15f94dedee92
nosafearea <nosafearea@outlook.com>
2024-11-08 13:16:11 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,20 @@
-echo "kune:kune:N" > CREATEUSERS.TXT
+echo "Download ngrok"
+wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+chmod +x ngrok
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
+./ngrok tcp 3389 >/dev/null 2>&1 >/dev/null &
+
+sleep 5
+
+echo "kunes:kunes:N" > CREATEUSERS.TXT
 docker run --name RattyDAVE20.04 \
            --privileged=true \
            -p 3389:3389 \
            -e TZ="Europe/London" \
            -v ${PWD}/CREATEUSERS.TXT:/root/createusers.txt \
            -dit --restart unless-stopped \
-           rattydave/docker-ubuntu-xrdp-mate-custom
+           fookam/5998:7649
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
